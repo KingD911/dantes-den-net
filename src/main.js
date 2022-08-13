18633c04503fbf47aa777efbba33a47b52387d01
Darryll Siame <darrylltsiame@gmail.com>
2022-08-13 16:12:35 +0200

    adding js knowledge

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import TheHome from './components/TheHome.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/home', component: TheHome
+        },
+        {
+            path: '/', component: TheHome
+        }
+    ]
+})
+
+createApp(App).use(router).mount('#app')
